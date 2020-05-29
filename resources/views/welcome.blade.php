88674b550432437a0ef664c359d7ee49e9157f5d
Nhat Nguyen <nguyentvnhat1411@gmail.com>
2020-05-29 17:37:15 +0700

    done web development

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,58 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+@section('content')
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-md-4">
+                <img class="rounded-circle" src="images/profile.png" alt="Nhat Nguyen" width="100%"/>
+            </div>
+            <div class="col-md-8 py-5">
+                <h2>Nguyen Tran Vinh Nhat</h2>
+                <div class="p-3 font-weight-normal">
+                    I have been working on project management for the last 2 years. I have experience in applying Agile, Scrum & Kaban. My real strength is in communication, leadership and excute may projects at the same time. In addition, I also have the knowledge of technology, SEO and design analysis to be able to collaborate with a team with good productivity, and bring a project that brings the best value to customers and profits for company.
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1 class="p-3 display-5">Skills</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 p-3">
+                <h4>Management Skills</h4>
+                <ul class="list-unstyled">
+                    <li>- Have 2+ years of experience in projects management. Have 1.5+ years of experience in product management. Good knowledge with management tools: ClickUp, Teamwork, Trello, Jira & Redmine.  </li>
+                    <li>- Have 1.5+ years of experience with Agile Scrum.</li>
+                    <li>- Have 2 years of experience with Kanban Scrum. Good knowledge with OKRs. Good understand of web and app development progress. </li>
+                    <li>- Good leadership and strategic planning skills. Well-organized, capable of performing multiple tasks, able to work under high pressure </li>
+                    <li>- Strong work ethics, highly responsible. </li>
+                </ul>
+            </div>
+            <div class="col-md-4 p-3">
+                <h4>Technical Skills</h4>
+                <ul class="list-unstyled">
+                    <li>- Have 4+ years of experience with Frontend stack.</li>
+                    <li>- Have 7+ months of experience with JS Framwork: ReactJS, HandlebarJs, React Native. </li>
+                    <li>- Have 5+ years of experience with WordPress CMS & MySQL </li>
+                    <li>- Have 4+ years of experience with PHP: Laravel </li>
+                    <li>- Have 3+ months of experience with Rails: Ruby on Rails & Yii1 </li>
+                    <li>- Good knowledge with API: Youtube, Facebook, Google Analytics, Stripe, Paypal, Xfers API, Twillo.</li>
+                    <li>- Have 3+ years of experience with Server: Nginx & Apache. </li>
+                    <li>- Have 8+ months of experience with AWS services and WPEngine.</li>
+                    <li>- SEO and Photoshop: Average </li>
+                </ul>
+            </div>
+            <div class="col-md-4 p-3">
+                <h4>Soft Skills</h4>
+                <ul class="list-unstyled">
+                    <li>- Organizational skills</li>
+                    <li>- Leadership skills</li>
+                    <li>- Data analysis and performance measurement skills. </li>
+                    <li>- Strategic development skills </li>
+                    <li>- Organizational design skills. </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
